9df35ac03e27daee71d59bf7d2e2d112ebb2e5dd
Todor Borisov <todor0203@gmail.com>
2023-11-09 03:15:43 +0200

    Added footer component.

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,29 @@
+import styles from './Footer.module.css';
+import { useEffect, useState } from 'react';
+
+export default function Footer() {
+
+    const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const newYear = new Date().getFullYear();
+            if (newYear !== currentYear) {
+                setCurrentYear(newYear);
+            }
+        }, 1000 * 60 * 60);
+        return () => clearInterval(interval);
+
+    }, [currentYear]);
+
+    return (
+        <footer className={styles['footer']}>
+            <p>Copyright &copy; {currentYear} Todor Borisov. All Rights Reserved. <span>Boats4U</span></p>
+
+            <div className={styles['social-media-icons']}>
+                <a href="https://github.com/TodorYBorisov/Boats4U-App-React"><i className="fa-brands fa-square-github"></i></a>
+                <a href="#"><i className="fa-brands fa-linkedin"></i></a>
+            </div>
+        </footer >
+    );
+}

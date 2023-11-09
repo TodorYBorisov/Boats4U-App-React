21aa6e7f11f68878076fab683bc48082a2354e4a
Todor Borisov <todor0203@gmail.com>
2023-11-09 03:17:21 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css';
 
+
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Team from './components/Team/Team';
+import Home from './components/Home/Home';
+
 function App() {
- 
+
 
   return (
     <>
-      
+      <Header />
+      <main className='main-container'>
+        <Home/>
+        <Team/>
+
+      </main>
+      <Footer />
     </>
   );
 }

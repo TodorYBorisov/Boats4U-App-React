7db11fef1672c02f88956b4272becf62f0731026
Todor Borisov <todor0203@gmail.com>
2023-11-09 22:51:51 +0200

    Update css files in navigation, team and footer

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -3,13 +3,15 @@ import styles from './Home.module.css';
 
 export default function Home() {
     return (
-        <div className={styles['site-home']}>
-        <section className={styles['header-title']}>
-                <h1>It's time to start your adventures!</h1>
-                <p>Integer blandit libero sit amet massa interdum fermentum</p>
-                <p>Veniam, facilis.</p>
-                <button>YOUR NEXT JOURNEY</button>
-            </section>
-        </div>
+        <main className='main-container'>
+            <div className={styles['site-home']}>
+                <section className={styles['header-title']}>
+                    <h1>It's time to start your adventures!</h1>
+                    <p>Integer blandit libero sit amet massa interdum fermentum</p>
+                    <p>Veniam, facilis.</p>
+                    <button>YOUR NEXT JOURNEY</button>
+                </section>
+            </div> 
+        </main>
     );
 }

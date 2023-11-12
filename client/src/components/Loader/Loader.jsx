874c4dc1b33efd96b9db07d82b10767a68a89f64
Todor Borisov <todor0203@gmail.com>
2023-11-12 19:51:03 +0200

    Loader component added to the catalog when fetching data.

---
+++ client/src/components/Loader/Loader.jsx
@@ -0,0 +1,10 @@
+import styles from './Loader.module.css';
+
+export default function Loader() {
+    return (
+        <div className={styles['loader']}>
+            <div className={styles['lds-dual-ring']}/>
+            <p>Loading...</p>
+        </div>
+    );
+}

643ea5e5e5858cdcff81515c03fd5b2ae9f8c2c6
Todor Borisov <todor0203@gmail.com>
2023-11-13 03:53:03 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,15 @@ import './App.css';
 
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 import Team from './components/Team/Team';
 import Home from './components/Home/Home';
 import Catalog from './components/Catalog/Catalog';
 import NotFound from './components/NotFound/NotFound';
 
 import Search1 from './components/Search1/Search1';
+import Details from './components/Details/Details';
 
 
 function App() {
@@ -26,6 +29,12 @@ function App() {
       <NotFound/>
       
       <Search1/>
+      
+      <Register/>
+
+      <Login/>
+
+      <Details/>
       
       <Footer />
     </>

d22e6e1cf3a5e959c2e46b3a46528e252053e382
Todor Borisov <todor0203@gmail.com>
2023-11-14 14:02:29 +0200

    Update navigation with routes.

--- client/src/components/Header/Navigation/Navigation.jsx
+++ client/src/components/Header/Navigation/Navigation.jsx
@@ -1,23 +1,24 @@
 import styles from './Navigation.module.css';
+import { Link } from 'react-router-dom';
 
 export default function Navigation() {
     return (
 
         <nav className={styles['main-nav']}>
 
-            <img src="/assets/logo.png" alt="site-logo" />
+            <Link to='/'><img src="/assets/logo.png" alt="site-logo" /></Link>
 
             <ul>
-                <li><a href="/"><i className="fa-solid fa-house"></i> Home</a></li>
-                {/* <li><a href="#"><i className="fa-solid fa-circle-info"></i> About Us</a></li> */}
-                <li><a href="#"><i className="fa-solid fa-anchor"></i> Boats</a></li>
-                <li><a href="#"><i className="fa-solid fa-circle-plus"></i> Create</a></li>
-                <li><a href="#"><i className="fa-solid fa-magnifying-glass"></i> Search</a></li>
+                <li><Link to="/"><i className="fa-solid fa-house"></i> Home</Link></li>
+                {/* <li><Link to='/about'><i className="fa-solid fa-circle-info"></i> About Us</Link></li> */}
+                <li><Link to="/boats"><i className="fa-solid fa-anchor"></i> Boats</Link></li>
+                <li><Link to="/create"><i className="fa-solid fa-circle-plus"></i> Create</Link></li>
+                <li><Link to="/search"><i className="fa-solid fa-magnifying-glass"></i> Search</Link></li>
                 <li><a href="#"><i className="fa-solid fa-cloud-sun"></i> Weather</a></li>
-                <li><a href="#"><i className="fa-solid fa-address-card"></i> Profile</a></li>
+                <li><Link to="/profile"><i className="fa-solid fa-address-card"></i> Profile</Link></li>
                 <li><a href="#"><i className="fa-solid fa-right-from-bracket"></i> Logout</a></li>
-                <li><a href="#"><i className="fa-solid fa-right-to-bracket"></i> Login</a></li>
-                <li><a href="#"><i className="fa-solid fa-user-plus"></i> Sign Up</a></li>
+                <li><Link to="/login"><i className="fa-solid fa-right-to-bracket"></i> Login</Link></li>
+                <li><Link to="/register"><i className="fa-solid fa-user-plus"></i> Sign Up</Link></li>
             </ul>
         </nav>
 

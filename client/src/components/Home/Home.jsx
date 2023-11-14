a91fd9020b5f22a298298e28c73a9464f40acbb4
Todor Borisov <todor0203@gmail.com>
2023-11-14 14:03:14 +0200

    Home page slight update.

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,15 +1,20 @@
 /* eslint-disable react/no-unescaped-entities */
+import { Link } from 'react-router-dom';
+
 import styles from './Home.module.css';
 
 export default function Home() {
+    document.title = 'Home';
+
     return (
-        <main className='main-container'>
+        <main className={styles['main-container']}>
             <div className={styles['site-home']}>
                 <section className={styles['header-title']}>
                     <h1>It's time to start your adventures!</h1>
-                    <p>Integer blandit libero sit amet massa interdum fermentum</p>
-                    <p>Veniam, facilis.</p>
-                    <button className={styles['button']}>YOUR NEXT JOURNEY</button>
+                    <p>We invite you to embark on your next adventure with us.</p>
+                    <p>Discover the freedom of the open sea, the beauty of the coastline, and the joy of yacht living</p>
+                    <Link to='/boats'><button className={styles['button']}>YOUR NEXT JOURNEY</button></Link>
+                    <Link to='/about'><button className={styles['button']}>ABOUT US</button></Link>
                 </section>
             </div> 
         </main>

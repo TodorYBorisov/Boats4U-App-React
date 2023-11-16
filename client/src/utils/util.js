f538e7fd370e0400404f4af9396d3046ba324371
Todor Borisov <todor0203@gmail.com>
2023-11-16 14:30:48 +0200

    Updated util services.

--- client/src/utils/util.js
+++ client/src/utils/util.js
@@ -1,35 +1,43 @@
-const itemName = 'userData';
+const auth = 'user';
 
 // взима потребителските данни
-export function getUserData() {
-    return JSON.parse(localStorage.getItem(itemName));
-}
+// export function getUserData() {
+//     return JSON.parse(localStorage.getItem(itemName));
+// }
 
 // запазваме потребителските данни в localStorage
 export function setUserData(data) {
-    return localStorage.setItem(itemName, JSON.stringify(data));
+    return localStorage.setItem(auth, JSON.stringify(data));
 }
 
 // изтриваме потребителските данни
 export function clearUserData() {
-    localStorage.removeItem(itemName);
+    localStorage.removeItem(auth);
 }
 
 
 // правим функция eventListenr on submit
 // получаваме данните от формуляря в data
-export function createSubmitHandler(callback) {
-    return function (event) {
-        event.preventDefault();
-        const form = event.currentTarget;
+// export function createSubmitHandler(callback) {
+//     return function (event) {
+//         event.preventDefault();
+//         const form = event.currentTarget;
 
-        const formData = new FormData(form);
-        const data = Object.fromEntries(formData.entries());
+//         const formData = new FormData(form);
+//         const data = Object.fromEntries(formData.entries());
 
-        //това тримва полетата в register i login
-        for (const input in data) {
-            data[input] = data[input].trim();
-        }
-        callback(data, form);
-    };
+//         //това тримва полетата в register i login
+//         for (const input in data) {
+//             data[input] = data[input].trim();
+//         }
+//         callback(data, form);
+//     };
+// }
+
+export function getUserData() {
+    return JSON.parse(localStorage.getItem(auth));
+}
+
+export async function isAuthenticated() {
+    return Boolean(getUserData());
 }

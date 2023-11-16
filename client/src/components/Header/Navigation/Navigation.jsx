22a0f98ec0a3c79d9958e6795a3afc6946df9330
Todor Borisov <todor0203@gmail.com>
2023-11-16 14:29:29 +0200

    Navigation changes based on the authentication status of the user.

--- client/src/components/Header/Navigation/Navigation.jsx
+++ client/src/components/Header/Navigation/Navigation.jsx
@@ -1,27 +1,55 @@
 import styles from './Navigation.module.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../../../context/authContext';
+import * as userServices from '../../../services/userServices';
+
 
 export default function Navigation() {
-    return (
 
-        <nav className={styles['main-nav']}>
+  const navigate = useNavigate();
+  const { auth, setAuth } = useContext(AuthContext);
 
-            <Link to='/'><img src="/assets/logo.png" alt="site-logo" /></Link>
+  async function logoutHandler(event) {
+    event.preventDefault();
 
-            <ul>
-                <li><Link to="/"><i className="fa-solid fa-house"></i> Home</Link></li>
-                {/* <li><Link to='/about'><i className="fa-solid fa-circle-info"></i> About Us</Link></li> */}
-                <li><Link to="/boats"><i className="fa-solid fa-anchor"></i> Boats</Link></li>
-                <li><Link to="/create"><i className="fa-solid fa-circle-plus"></i> Create</Link></li>
-                <li><Link to="/search"><i className="fa-solid fa-magnifying-glass"></i> Search</Link></li>
-                <li><a href="#"><i className="fa-solid fa-cloud-sun"></i> Weather</a></li>
-                <li><Link to="/profile"><i className="fa-solid fa-address-card"></i> Profile</Link></li>
-                <li><a href="#"><i className="fa-solid fa-right-from-bracket"></i> Logout</a></li>
-                <li><Link to="/login"><i className="fa-solid fa-right-to-bracket"></i> Login</Link></li>
-                <li><Link to="/register"><i className="fa-solid fa-user-plus"></i> Sign Up</Link></li>
-            </ul>
-        </nav>
+    try {
+      await userServices.logout(auth.accessToken);
+      setAuth(null);
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-    );
+  return (
+
+    <nav className={styles['main-nav']}>
+
+      <Link to='/'><img src="/assets/logo.png" alt="site-logo" /></Link>
+
+      <ul>
+        <li><Link to="/"><i className="fa-solid fa-house"></i> Home</Link></li>
+        {/* <li><Link to='/about'><i className="fa-solid fa-circle-info"></i> About Us</Link></li> */}
+        <li><Link to="/boats"><i className="fa-solid fa-anchor"></i> Boats</Link></li>
+
+        {auth ?
+          <>
+            <li><Link to="/create"><i className="fa-solid fa-circle-plus"></i> Create</Link></li>
+            <li><Link to="/search"><i className="fa-solid fa-magnifying-glass"></i> Search</Link></li>
+            <li><a href="#"><i className="fa-solid fa-cloud-sun"></i> Weather</a></li>
+            <li><Link to="/profile"><i className="fa-solid fa-address-card"></i> Profile</Link></li>
+            <li onClick={logoutHandler}><Link><i className="fa-solid fa-right-from-bracket"></i> Logout</Link></li>
+          </>
+          :
+          <>
+            <li><Link to="/login"><i className="fa-solid fa-right-to-bracket"></i> Login</Link></li>
+            <li><Link to="/register"><i className="fa-solid fa-user-plus"></i> Sign Up</Link></li>
+          </>
+        }
+      </ul>
+    </nav>
+
+  );
 }
 

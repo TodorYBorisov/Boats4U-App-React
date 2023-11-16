360436a9400cfdff21327ec1dfe7f926f2165b48
Todor Borisov <todor0203@gmail.com>
2023-11-16 14:31:40 +0200

    Update Login.jsx

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,17 +1,52 @@
 import styles from './Login.module.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../../context/authContext';
+import * as userServices from '../../services/userServices';
+
+
 
 export default function Login() {
     document.title = 'Login';
 
+    const navigate = useNavigate();
+    const { setAuth } = useContext(AuthContext);
 
+    // async function onSubmit(event) {
+    //     event.preventDefault();
+    //     const { email, password } = Object.fromEntries(new FormData(event.target));
+
+    //     try {
+    //         const user = await userServices.login(email, password);
+    //         setAuth(user);
+    //         navigate('/');
+
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
+
+    function onSubmit(event) {
+        event.preventDefault();
+        const { email, password } = Object.fromEntries(new FormData(event.target));
+    
+        userServices.login(email, password)
+            .then(auth => {
+                setAuth(auth);
+                navigate('/');
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+    
     return (
         <div className={styles['login']}>
 
             <div className={styles['login-box']}>
                 <h1>Login</h1>
 
-                <form>
+                <form onSubmit={onSubmit}>
                     <label htmlFor="email"><span><i className="fa-solid fa-envelope"></i></span>Email</label>
                     <input type="email" name="email" placeholder="Email.." />
 

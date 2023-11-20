a68adc34563648eead6fe0b83133e76f5b966e73
Todor Borisov <todor0203@gmail.com>
2023-11-20 23:20:46 +0200

    Update Register.jsx

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -48,44 +48,19 @@ export default function Register() {
                     <label htmlFor="username"><span><i className="fa-solid fa-user"></i></span>Username</label>
                     <input onChange={onChange} value={userData.username} type="text" name="username" placeholder="Username.." />
 
-                    <label htmlFor="email">
-                        <span>
-                            <i className="fa-solid fa-envelope"></i>
-                        </span>
-                        Email
-                    </label>
+                    <label htmlFor="email"><span><i className="fa-solid fa-envelope"></i></span>Email</label>
                     <input onChange={onChange} value={userData.email} type="email" name="email" placeholder="Email.." />
 
-                    <label htmlFor="phone">
-                        <span>
-                            <i className="fa-solid fa-phone-volume"></i>
-                        </span>
-                        Phone
-                    </label>
+                    <label htmlFor="phone"><span><i className="fa-solid fa-phone-volume"></i></span>Phone</label>
                     <input onChange={onChange} value={userData.phone} type="text" name="phone" id="phone" placeholder="+359 88 888 888" />
 
-                    <label htmlFor="password">
-                        <span>
-                            <i className="fa-solid fa-lock"></i>
-                        </span>
-                        Password
-                    </label>
+                    <label htmlFor="password"><span><i className="fa-solid fa-lock"></i></span>Password</label>
                     <input onChange={onChange} value={userData.password} type="password" name="password" placeholder="Password.." />
 
-                    <label htmlFor="rePassword">
-                        <span>
-                            <i className="fa-solid fa-lock"></i>
-                        </span>
-                        Confirm Password
-                    </label>
+                    <label htmlFor="rePassword"><span><i className="fa-solid fa-lock"></i></span>Confirm Password</label>
                     <input onChange={onChange} value={userData.rePassword} type="password" name="rePassword" placeholder="Confirm Password.." />
 
-                    <label htmlFor="gender">
-                        <span>
-                            <i className="fa-solid fa-venus-mars"></i>
-                        </span>
-                        Gender
-                    </label>
+                    <label htmlFor="gender"><span><i className="fa-solid fa-venus-mars"></i></span>Gender</label>
                     <div className={styles['gender']}>
                         <input onChange={onChange} checked={userData.gender === 'female'} type="radio" id="female" name="gender" value="female" />
                         <label htmlFor="female">Female</label>

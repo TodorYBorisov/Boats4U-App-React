52d215cdfa17f6f9c019a3ca5d5f7e2155b0af4b
Todor Borisov <todor0203@gmail.com>
2023-11-20 23:43:06 +0200

    Update Navigation.jsx

--- client/src/components/Header/Navigation/Navigation.jsx
+++ client/src/components/Header/Navigation/Navigation.jsx
@@ -27,7 +27,6 @@ export default function Navigation() {
     <nav className={styles['main-nav']}>
 
       <Link to='/'><img src="/assets/logo.png" alt="site-logo" /></Link>
-
       <ul>
         <li><Link to="/"><i className="fa-solid fa-house"></i> Home</Link></li>
         {/* <li><Link to='/about'><i className="fa-solid fa-circle-info"></i> About Us</Link></li> */}
@@ -40,6 +39,7 @@ export default function Navigation() {
             <li><Link to='/weather'><i className="fa-solid fa-cloud-sun"></i> Weather</Link></li>
             <li><Link to="/profile"><i className="fa-solid fa-address-card"></i> Profile</Link></li>
             <li onClick={logoutHandler}><Link><i className="fa-solid fa-right-from-bracket"></i> Logout</Link></li>
+            <p>Welcome, {auth.username}</p>
           </>
           :
           <>

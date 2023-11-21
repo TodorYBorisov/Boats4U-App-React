7a6ff6ae0222686bf4c747c2a342b672616b0362
Todor Borisov <todor0203@gmail.com>
2023-11-22 00:11:22 +0200

    Update util.js

--- client/src/utils/util.js
+++ client/src/utils/util.js
@@ -16,24 +16,6 @@ export function clearUserData() {
 }
 
 
-// правим функция eventListenr on submit
-// получаваме данните от формуляря в data
-// export function createSubmitHandler(callback) {
-//     return function (event) {
-//         event.preventDefault();
-//         const form = event.currentTarget;
-
-//         const formData = new FormData(form);
-//         const data = Object.fromEntries(formData.entries());
-
-//         //това тримва полетата в register i login
-//         for (const input in data) {
-//             data[input] = data[input].trim();
-//         }
-//         callback(data, form);
-//     };
-// }
-
 export function getUserData() {
     return JSON.parse(localStorage.getItem(auth));
 }

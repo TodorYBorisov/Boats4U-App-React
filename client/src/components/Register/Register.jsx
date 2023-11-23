51ea61c2fa856dd399f59f7b363567894c64b677
Todor Borisov <todor0203@gmail.com>
2023-11-23 23:01:22 +0200

    Added validation to form fields in the register component.
    Added styles to server errors.

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -20,6 +20,10 @@ export default function Register() {
         rePassword: '',
     });
 
+    const [errors, setErrors] = useState({});
+    // const [hasServerError, setHasServerError] = useState(false);
+    // const [serverError, setServerError] = useState({});
+
     function onChange(event) {
         setuserData(state => ({ ...state, [event.target.name]: event.target.value }));
     }
@@ -35,32 +39,97 @@ export default function Register() {
             navigate('/');
         } catch (error) {
             console.log(error);
+            // setHasServerError(true);
+            // setServerError(error.message);
         }
     }
+
+    const usernameValidator = () => {
+        if (userData.username.length < 3) {
+            setErrors(state => ({ ...state, username: 'Username must be at least 3 characters long!' }));
+        } else {
+            if (errors.username) {
+                setErrors(state => ({ ...state, username: '' }));
+            }
+        }
+    };
+
+    function validateEmail(email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+
+    const emailValidator = () => {
+        if (!validateEmail(userData.email)) {
+            setErrors(state => ({
+                ...state,
+                email: 'Please enter a valid email address!'
+            }));
+        } else {
+            if (errors.email) {
+                setErrors(state => ({ ...state, email: '' }));
+            }
+        }
+    };
+
+    const phoneValidator = () => {
+        if (!Number(userData.phone)) {
+            setErrors(state => ({ ...state, phone: 'Please, enter your Phone Number!' }));
+        } else {
+            if (errors.phone) {
+                setErrors(state => ({ ...state, phone: '' }));
+            }
+        }
+    };
+
+    const passwordValidator = () => {
+        if (userData.password.length < 5) {
+            setErrors(state => ({ ...state, password: 'Password must be at least 5 characters long!' }));
+        } else {
+            if (errors.password) {
+                setErrors(state => ({ ...state, password: '' }));
+            }
+        }
+    };
+
+    const rePasswordValidator = () => {
+        if (userData.rePassword != userData.password) {
+            setErrors(state => ({ ...state, rePassword: 'Passwords do not match. Please try again!' }));
+        } else {
+            if (errors.rePassword) {
+                setErrors(state => ({ ...state, rePassword: '' }));
+            }
+        }
+    };
+
 
     return (
         <div className={styles['register']}>
             <div className={styles['register-box']}>
                 <h1>Sign Up</h1>
-                {/* <h4>It's free and only take a minute</h4> */}
 
                 <form onSubmit={onSubmit}>
                     <label htmlFor="username"><span><i className="fa-solid fa-user"></i></span>Username</label>
-                    <input onChange={onChange} value={userData.username} type="text" name="username" placeholder="Username.." />
+                    <input onChange={onChange} value={userData.username} onBlur={usernameValidator} type="text" name="username" placeholder="Username.." />
+                    {errors.username && (<p className={styles['errorMessage']}>{errors.username}</p>)}
 
                     <label htmlFor="email"><span><i className="fa-solid fa-envelope"></i></span>Email</label>
-                    <input onChange={onChange} value={userData.email} type="email" name="email" placeholder="Email.." />
+                    <input onChange={onChange} value={userData.email} onBlur={emailValidator} type="email" name="email" placeholder="example@example.com" />
+                    {errors.email && (<p className={styles['errorMessage']}>{errors.email}</p>)}
 
                     <label htmlFor="phone"><span><i className="fa-solid fa-phone-volume"></i></span>Phone</label>
-                    <input onChange={onChange} value={userData.phone} type="text" name="phone" id="phone" placeholder="+359 88 888 888" />
+                    <input onChange={onChange} value={userData.phone} onBlur={phoneValidator} type="text" name="phone" id="phone" placeholder="+359 88 888 888" />
+                    {errors.phone && (<p className={styles['errorMessage']}>{errors.phone}</p>)}
 
                     <label htmlFor="password"><span><i className="fa-solid fa-lock"></i></span>Password</label>
-                    <input onChange={onChange} value={userData.password} type="password" name="password" placeholder="Password.." />
+                    <input onChange={onChange} value={userData.password} onBlur={passwordValidator} type="password" name="password" placeholder="Password.." />
+                    {errors.password && (<p className={styles['errorMessage']}>{errors.password}</p>)}
 
                     <label htmlFor="rePassword"><span><i className="fa-solid fa-lock"></i></span>Confirm Password</label>
-                    <input onChange={onChange} value={userData.rePassword} type="password" name="rePassword" placeholder="Confirm Password.." />
+                    <input onChange={onChange} value={userData.rePassword} onBlur={rePasswordValidator} type="password" name="rePassword" placeholder="Confirm Password.." />
+                    {errors.rePassword && (<p className={styles['errorMessage']}>{errors.rePassword}</p>)}
 
-                    <label htmlFor="gender"><span><i className="fa-solid fa-venus-mars"></i></span>Gender</label>
+                    <label htmlFor="gender"><span><i className="fa-solid fa-venus-mars"></i></span>Select Gender</label>
                     <div className={styles['gender']}>
                         <input onChange={onChange} checked={userData.gender === 'female'} type="radio" id="female" name="gender" value="female" />
                         <label htmlFor="female">Female</label>
@@ -68,8 +137,11 @@ export default function Register() {
                         <label htmlFor="male">Male</label>
                     </div>
 
-                    <button className={styles['button-register']}>Sign Up</button>
-                    {/* <input type="submit" value="Register" /> */}
+                    <button className={styles['button-register']} disabled={(Object.values(errors).some(x => x)
+                        || (Object.values(userData).some(x => x == '')))}>Sign Up</button>
+
+                    {/* {hasServerError && (<p className={styles.serverError}>{serverError}</p>)} */}
+
                 </form>
 
                 <div className={styles['last-p']}>

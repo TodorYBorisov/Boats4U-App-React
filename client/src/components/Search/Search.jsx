aa212ce0f783b2fc01ceb43391ddadb91fa2cb77
Todor Borisov <todor0203@gmail.com>
2023-11-26 11:23:34 +0200

    Search functionality implemented in the search component!
    Logged in users can search by various criteria, starting point, ending point, model, description, price, year, date, even a partial word contained in the description of additional information.

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,20 +1,105 @@
 import styles from './Search.module.css';
-import { useState } from 'react';
+import {useState } from 'react';//useEffect
+import * as dataService from '../../services/dataService';
+import OfferItem from '../Catalog/OfferList/OfferItem/OfferItem';
+import Loader from '../Loader/Loader';
 
-export default function Search(searchHandler) {
+const initialValue = {
+    search: ''
+};
 
-    const [form, setForm] = useState({
-        search: ''
-    });
+export default function Search() {
+    const [errors, setErrors] = useState({});
+    const [boats, setBoats] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [searchValue, setSearchValue] = useState(initialValue);
+    const [searchPerformed, setSearchPerformed] = useState(false);
+
+    //If i want instant search i can implement this below!
+    // useEffect(() => {
+    //     if (searchValue.search.trim() === '') {
+    //         setBoats([]);
+    //         setSearchPerformed(false);
+    //         return;
+    //     }
+    //     setLoading(true);
+    //     dataService.getAllData()
+    //         .then(result => {
+    //             const filteredBoats = result.filter(boat =>
+    //                 boat.startPoint.toLowerCase().includes(searchValue.search.toLowerCase()) ||
+    //                 boat.endPoint.toLowerCase().includes(searchValue.search.toLowerCase()) ||
+    //                 boat.model.toLowerCase().includes(searchValue.search.toLowerCase()) ||
+    //                 boat.description.toLowerCase().includes(searchValue.search.toLowerCase()) ||
+    //                 (typeof boat.price === 'number' &&
+    //                     boat.price.toString().includes(searchValue.search.toLowerCase())) ||
+    //                 (typeof boat.year === 'number' &&
+    //                     boat.year.toString().includes(searchValue.search.toLowerCase())) ||
+    //                 (typeof boat.date === 'string' &&
+    //                 boat.date.includes(searchValue.search.toLowerCase()))
+    //             );
+    //             setBoats(filteredBoats);
+
+    //             if (filteredBoats.length === 0) {
+    //                 setSearchPerformed(true);
+    //             } else {
+    //                 setSearchPerformed(false);
+    //             }
+    //         })
+    //         .catch(error => console.log(error))
+    //         .finally(() => setLoading(false));
+    // }, [searchValue]);
+
+    const onChange = (event) => {
+        setSearchValue(state => ({
+            ...state,
+            [event.target.name]: event.target.value,
+        }));
+    };
+    //uncomment for instant search and replace onsubmit with handleSearch
+    // const handleSearch = () => {
+    // };
 
     function onSubmit(event) {
         event.preventDefault();
-        searchHandler(form);
+
+        setLoading(true);
+        dataService.getAllData()
+            .then(result => {
+                const filteredBoats = result.filter(boat =>
+                    boat.startPoint.toLowerCase().includes(searchValue.search.toLowerCase()) ||
+                    boat.endPoint.toLowerCase().includes(searchValue.search.toLowerCase()) ||
+                    boat.model.toLowerCase().includes(searchValue.search.toLowerCase()) ||
+                    boat.description.toLowerCase().includes(searchValue.search.toLowerCase()) ||
+                    (typeof boat.price === 'number' &&
+                        boat.price.toString().includes(searchValue.search.toLowerCase())) ||
+                    (typeof boat.year === 'number' &&
+                        boat.year.toString().includes(searchValue.search.toLowerCase())) ||
+                    (typeof boat.date === 'string' &&
+                        boat.date.includes(searchValue.search.toLowerCase())
+                        )
+                );
+
+                setBoats(filteredBoats);
+
+                if (filteredBoats.length === 0) {
+                    setSearchPerformed(true);
+                } else {
+                    setSearchPerformed(false);
+                }
+            })
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false));
     }
 
-    function onChange(event) {
-        setForm(state => ({ ...state, [event.target.name]: event.target.value }));
-    }
+    const inputValidator = () => {
+        if (searchValue.search.length < 1) {
+            setErrors(state => ({ ...state, search: 'The search field cannot be empty!' }));
+        } else {
+            if (errors.search) {
+                setErrors(state => ({ ...state, search: '' }));
+            }
+        }
+    };
 
     return (
         <section className={styles['search-page']}>
@@ -23,34 +108,71 @@ export default function Search(searchHandler) {
 
             <section className={styles['search-container']}>
 
-                <form className={styles['search-form']} onSubmit={onSubmit}>
+                <form className={styles['search-form']} >
                     <input
-                        value={form.search}
+                        value={searchValue.search}
                         name="search"
                         type="text"
                         placeholder="Search here..."
                         onChange={onChange}
-                    />
-
-                    <button className={styles['search-button']} type="submit">
+                        onBlur={inputValidator}
+                        />
+                        {errors.search && (<p className={styles['errorMessage']}>{errors.search}</p>)}
+                    <button className={styles['search-button']} disabled={Object.values(errors).some(x=>x) || (Object.values(searchValue).some(x => x == ''))} type="submit" onClick={onSubmit}>
                         <i className="fa-solid fa-magnifying-glass"></i>
                     </button>
                 </form>
             </section>
 
-            {/* 
-                {loading ?
-                    <RingLoader color="#36d7b7" />
-                    :
-                    <OffersList offers={offers} />
-                } */}
-
-            {/* <OffersList /> */}
-
-            {/* <div className={styles['no-boats']}>
-                <h2>Sorry, no boats found for you.</h2>
-                <h2>Please visit us again later!</h2>
-            </div> */}
+            {loading ?
+                <Loader />
+                : <div className={styles['offersList-main-section']}>
+                    {boats.length > 0
+                        ? <ul className={styles['offersList-section']}>
+                            {boats.map(boat => (
+                                <li key={boat._id}>
+                                    <OfferItem
+                                        id={boat._id}
+                                        imageUrl={boat.imageUrl}
+                                        startPoint={boat.startPoint}
+                                        date={boat.date}
+                                        time={boat.time}
+                                        price={boat.price}
+                                        model={boat.model}
+                                    />
+                                </li>
+                            ))}
+                        </ul>
+                        : <div className={styles['no-boats']}>
+                            {searchPerformed && (<h2>Sorry, no results were found!</h2>)}
+                        </div>
+                    }
+                </div>}
         </section>
     );
 }
+
+
+    // function onChange(event) {
+    //     setSearchValue(state => ({ ...state, [event.target.name]: event.target.value }));
+    // }
+
+    // function onChange(event) {
+    //     const { name, value } = event.target;
+    //     setBoats((prevFormData) => ({ ...prevFormData, [name]: value, }));
+    // }
+
+
+
+        // const onChange = (event) => {
+    //     let value = '';
+    //     if (event.target.startPoint) {
+    //         value = event.target.value;
+    //     }
+
+    //     setSearchValue(state => ({
+    //         ...state,
+    //         [event.target.name]: value,
+    //     }));
+
+    // };

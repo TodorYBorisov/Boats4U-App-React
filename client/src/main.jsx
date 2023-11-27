e5d3f532acdaa9a7982108bb381f276099d475ed
Todor Borisov <todor0203@gmail.com>
2023-11-27 23:36:54 +0200

    Uncommented the strict mode in main.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,9 +7,9 @@ import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    // <React.StrictMode>
+     <React.StrictMode>
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    // </React.StrictMode>,
+     </React.StrictMode>,
 );

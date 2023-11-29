41cbfca3f440b6994575d14c54c9c8fa28a9f22d
Todor Borisov <todor0203@gmail.com>
2023-11-29 13:57:24 +0200

    Cleaned up and updated user services, removing old unused features.

--- client/src/services/userServices.js
+++ client/src/services/userServices.js
@@ -1,27 +1,11 @@
 import { clearUserData} from '../utils/util.js';
 import { get, post } from './apiServices.js';
 
-// TODO да проверя пътя дали е такъв в документа с условието
 const endpoints = {
     login: '/users/login',
     register: '/users/register',
     logout: '/users/logout'
 };
-
-// TODO да се смени юзър обекта{} може да бъде (username, email, avatar, password)
-// export async function login(email, password) {
-//     const result = await post(endpoints.login, { email, password });
-//     setUserData(result);
-// }
-
-// TODO тук да видя как е записано от html name дали e repasss е правилно, ако трябва подаваме repass
-// export async function register(username, email, phone, gender, password) {
-//     const result = await post(endpoints.register, { username, email, phone, gender, password });
-//     setUserData(result);
-// }
-
-
-// ==================================================================================
 
 export async function register(username, email, phone, gender, password) {
     const response = await post(endpoints.register, { username, email, phone, gender, password });
@@ -38,14 +22,9 @@ export async function logout() {
     clearUserData();
 }
 
-
 export const getUser = () => {
     let username = localStorage.getItem('user');
     
     console.log(username);
     return username;
 };
-
-// export const isAuthenticated = () => {
-//     return Boolean(getUser());
-// };
--- client/src/utils/util.js
+++ client/src/utils/util.js
@@ -1,20 +1,16 @@
 const auth = 'user';
 
-// взима потребителските данни
 // export function getUserData() {
 //     return JSON.parse(localStorage.getItem(itemName));
 // }
 
-// запазваме потребителските данни в localStorage
 export function setUserData(data) {
     return localStorage.setItem(auth, JSON.stringify(data));
 }
 
-// изтриваме потребителските данни
 export function clearUserData() {
     localStorage.removeItem(auth);
 }
-
 
 export function getUserData() {
     return JSON.parse(localStorage.getItem(auth));

e2368cdb69db15b5cb9721d1cb36209c2221f26c
Todor Borisov <todor0203@gmail.com>
2023-12-01 10:49:32 +0200

    Import Error boundary which covers the whole app.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-
 import App from './App.jsx';
 import './index.css';
+import ErrorBoundary from './components/Guards/ErrorBoundary.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-     </React.StrictMode>,
+    <React.StrictMode>
+        <ErrorBoundary>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ErrorBoundary>
+    </React.StrictMode>,
 );

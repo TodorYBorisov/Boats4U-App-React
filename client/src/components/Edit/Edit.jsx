a197fd41de636d02a84d023884b7488fe069ff30
Todor Borisov <todor0203@gmail.com>
2023-12-06 00:41:47 +0200

    Edit component and css file updated.

--- client/src/components/Edit/Edit.jsx
+++ client/src/components/Edit/Edit.jsx
@@ -6,9 +6,7 @@ import { useState, useEffect } from 'react';
 export default function Edit() {
     document.title = 'Edit';
 
-
     const navigate = useNavigate();
-    // const [disableButton, setDisableButton] = useState(false);
     const [formData, setFormData] = useState({
         startPoint: '',
         endPoint: '',
@@ -25,19 +23,17 @@ export default function Edit() {
     const { id } = useParams();
     const [errors, setErrors] = useState({});
 
-      useEffect(() => {
+    useEffect(() => {
         dataService.getBoatById(id)
             .then(result => setFormData(result))
             .catch((error) => console.log(error));
     }, [id]);
-  
-    //тук актуализираме стария стейт на формата при промяна в input полето
+
     function onChange(event) {
         const { name, value } = event.target;
         setFormData((prevFormData) => ({ ...prevFormData, [name]: value, }));
     }
 
-    //onSubmit Edit
     const onSubmit = (event) => {
         event.preventDefault();
 
@@ -50,7 +46,6 @@ export default function Edit() {
             });
     };
 
-    //=====================================Validations
     const startPointValidator = () => {
         if (formData.startPoint.length < 1) {
             setErrors(state => ({ ...state, startPoint: 'The starting point must be at least 1 character long!' }));
@@ -161,7 +156,7 @@ export default function Edit() {
 
     const descriptionValidator = () => {
         if (formData.description.length < 20) {
-            setErrors(state => ({...state,description: 'Additional descriptive information must be at least 20 characters long!'}));
+            setErrors(state => ({ ...state, description: 'Additional descriptive information must be at least 20 characters long!' }));
         } else {
             if (errors.description) {
                 setErrors(state => ({ ...state, description: '' }));
@@ -169,62 +164,12 @@ export default function Edit() {
         }
     };
 
-    //=====================================
-
     return (
         <>
             <section className={styles['py-5']} id={styles['offer-trip-page']}>
                 <div className={`${styles['container']} ${styles['offer-trip']}`}>
                     <h1>Edit trip</h1>
                     <div>
-                        {/* <form onSubmit={onSubmit} >
-                            <div className={styles['offer-label']}>
-                                <label htmlFor="startPoint"> <i className="fas fa-map-marker-alt"></i> Starting Port</label>
-                                <label htmlFor="endPoint"> <i className="fas fa-map-marker-alt"></i> End Port</label>
-                            </div>
-                            <div className={`${styles['form-group']} ${styles['offer-input']}`}>
-                                <input onChange={onChange} type="text" className={styles['form-control-2']} id="startPoint" placeholder="Corfu" name="startPoint" value={formData.startPoint} />
-                                <input onChange={onChange} type="text" className={styles['form-control-2']} id="endPoint" placeholder="Zakynthos" name="endPoint" value={formData.endPoint} />
-                            </div>
-                            <div className={styles['offer-label']}>
-                                <label htmlFor="date"> <i className="far fa-calendar-alt"></i> Start Date</label>
-                                <label htmlFor="time"> <i className="far fa-clock"></i> Start Time</label>
-                            </div>
-                            <div className={`${styles['form-group']} ${styles['offer-input']}`}>
-                                <input onChange={onChange} type="date" className={styles['form-control-2']} id="date" placeholder="09 Dec 2023" name="date" value={formData.date} />
-                                <input onChange={onChange} type="time" className={styles['form-control-2']} id="time" placeholder="08:00 PM" name="time" value={formData.time} />
-                            </div>
-                            <div className={styles['offer-label']}>
-                                <label htmlFor="imageUrl"> <i className="fa-solid fa-image"></i> Image</label>
-                                <label htmlFor="model"> <i className="fa-solid fa-circle-info"></i> Model</label>
-                            </div>
-                            <div className={`${styles['form-group']} ${styles['offer-input']}`}>
-                                <input onChange={onChange} type="text" className={styles['form-control-2']} id="imageUrl" placeholder="https://..." name="imageUrl" value={formData.imageUrl} />
-                                <input onChange={onChange} type="text" className={styles['form-control-2']} id="model" placeholder="Jeanneau Leader" name="model" value={formData.model} />
-                            </div>
-                            <div className={styles['offer-label']}>
-                                <label htmlFor="passengerCapacity"> <i className="fa-solid fa-ship"></i> Capacity</label>
-                                <label htmlFor="price"> <i className="fa-solid fa-euro-sign"></i> Price</label>
-                            </div>
-                            <div className={`${styles['form-group']} ${styles['offer-input']}`}>
-                                <input onChange={onChange} type="text" className={styles['form-control-2']} id="passengerCapacity" placeholder="10" name="passengerCapacity" value={formData.passengerCapacity} />
-                                <input onChange={onChange} type="text" className={styles['form-control-2']} id="price" placeholder="2000" name="price" value={formData.price} />
-                            </div>
-
-                            <div className={styles['offer-label']}>
-                                <label htmlFor="availability">Availability</label>
-                                <label htmlFor="year">Year</label>
-                            </div>
-                            <div className={`${styles['form-group']} ${styles['offer-input']}`}>
-                                <input onChange={onChange} type="text" className={styles['form-control-2']} id="availability" placeholder="4" name="availability" value={formData.availability} />
-                                <input onChange={onChange} type="text" className={styles['form-control-2']} id="year" placeholder="2022" name="year" value={formData.year} />
-                            </div>
-                            <div className={styles['form-group']}>
-                                <label htmlFor="description">Description</label>
-                                <textarea onChange={onChange} className={styles['form-control']} id={styles['description']} placeholder="Any additional information about the trip" name="description" value={formData.description}></textarea>
-                            </div>
-                            <button type="submit" className={`${styles['btn']} ${styles['btn-primary']}`}>SUBMIT</button>
-                        </form> */}
                         <form onSubmit={onSubmit} >
                             <div className={styles['offer-label']}>
                                 <label htmlFor="startPoint"> <i className="fas fa-map-marker-alt"></i> Starting Port</label>
@@ -274,7 +219,7 @@ export default function Edit() {
                             <div className={`${styles['form-group']} ${styles['offer-input']}`}>
                                 <input onChange={onChange} onBlur={availabilityValidator} type="number" className={styles['form-control-2']} id="availability" placeholder="4" name="availability" value={formData.availability} />
                                 {errors.availability && (<p className={styles['errorMessage']}>{errors.availability}</p>)}
-                                {errors.year && (<p className={styles['errorMessage']}>{errors.year}</p>)}  
+                                {errors.year && (<p className={styles['errorMessage']}>{errors.year}</p>)}
                                 <input onChange={onChange} onBlur={yearValidator} type="number" className={styles['form-control-2']} id="year" placeholder="2022" name="year" value={formData.year} />
                             </div>
                             <div className={styles['form-group']}>
@@ -283,12 +228,11 @@ export default function Edit() {
                                 <textarea onChange={onChange} onBlur={descriptionValidator} className={styles['form-control']} id={styles['description']} placeholder="Any additional information about the trip" name="description" value={formData.description}></textarea>
                             </div>
                             <button disabled={(Object.values(errors).some(x => x)
-                                            || (Object.values(formData).some(x => x == '')))} type="submit" className={`${styles['btn']} ${styles['btn-primary']}`}>SUBMIT</button>
+                                || (Object.values(formData).some(x => x == '')))} type="submit" className={`${styles['btn']} ${styles['btn-primary']}`}>SUBMIT</button>
                         </form>
                     </div>
                 </div>
             </section>
-
         </>
     );
 }

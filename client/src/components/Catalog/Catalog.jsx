01c7a94956931c8d6fe8dd269efda55baaf4d81c
Todor Borisov <todor0203@gmail.com>
2023-12-06 00:57:04 +0200

    Catalog component and CSS file have been revised and updated.

--- client/src/components/Catalog/Catalog.jsx
+++ client/src/components/Catalog/Catalog.jsx
@@ -1,19 +1,13 @@
 import * as dataService from '../../services/dataService';
-
 import styles from './Catalog.module.css';
-// import OffersList from './OfferList/OffersList';
-
 import { useState, useEffect } from 'react';
 import OfferItem from './OfferList/OfferItem/OfferItem';
 import Loader from '../Loader/Loader.jsx';
-
 
 export default function Catalog() {
     document.title = 'Boats';
     const [boats, setBoats] = useState([]);
     const [loading, setLoading] = useState(false);
-
-    // console.log(boats);
 
     useEffect(() => {
         setLoading(true);
@@ -28,24 +22,12 @@ export default function Catalog() {
             .finally(()=> setLoading(false));
     }, []);
 
-    // useEffect(() => {
-    //     fetch('http://localhost:3030/data/boats')
-    //         .then((responce) => responce.json())
-    //         .then((data) => {
-    //             setBoats(data);
-
-    //         }).catch((error)=>{
-    //             console.log(error);
-    //         });
-    // }, []);
-
     return (
         <>
             <section className={styles['catalog-page']}>
 
                 <h1 className={styles['title-catalog']}>Catalog</h1>
 
-                {/* <OffersList boats={boats} /> */}
                 {loading ?
                     <Loader />
                     : <div className={styles['offersList-main-section']}>
@@ -71,27 +53,6 @@ export default function Catalog() {
                             </div>
                         }
                     </div>}
-                {/* <div className={styles['offersList-main-section']}>
-                    {boats.length > 0
-                        ? <ul className={styles['offersList-section']}>
-                            {boats.map(boat => (
-                                <li key={boat._id}>
-                                    <OfferItem
-                                        imageUrl={boat.imageUrl}
-                                        startPoint={boat.startPoint}
-                                        date={boat.date}
-                                        time={boat.time}
-                                        price={boat.price}
-                                    />
-                                </li>
-                            ))}
-                        </ul>
-                        : <div className={styles['no-boats']}>
-                            <h2>Unfortunately, we currently have no boats available for you right now...</h2>
-                            <h2>Please visit us again later!</h2>
-                        </div>
-                    }
-                </div> */}
             </section>
         </>
     );
